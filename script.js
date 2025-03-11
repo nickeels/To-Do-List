64062bdd991437e7552425ed34b4962d6f0b0dee
nickeels <46nikhilbahadur@gmail.com>
2025-03-11 21:34:53 +0000

    script.js

--- script.js
+++ script.js
@@ -4,68 +4,90 @@ const taskList = document.getElementById("task-list");
 
 let tasks = fetchTasks();
 
-taskForm.addEventListener("submit", function(e) {    
+taskForm.addEventListener("submit", function(e) {
     e.preventDefault();
     addTask();
     console.log(tasks);
 })
 
-// add task
+// Add task
 function addTask() {
-    const task = inputBox.value.trim()
-    if (task === "") {
-        alert("Task must be 0> characters long!")
-    }
-    else {
+    const taskDesc = inputBox.value.trim();
+
+    if (taskDesc === "") {
+        alert("Task must be greater than 0 characters long!");
+    } else {
+        const task = {
+            description: taskDesc,
+            completed: false
+        };
         tasks.push(task);
         updateTask();
         storeTasks();
-        inputBox.value = "";
 
+        inputBox.value = "";
     }
 }
- 
-function createTask(taskDesc, taskIndex) {
-    const taskId = "task-"+taskIndex;
-    const task = document.createElement("li");
-    task.className = "task";
-    task.innerHTML = `
+
+function createTask(task, taskIndex) {
+    const taskId = "task-" + taskIndex;
+    const taskItem = document.createElement("li");
+    
+    taskItem.className = "task";
+    taskItem.innerHTML = `
         <input type="checkbox" id="${taskId}">
-        <p2>${taskDesc}</p2>
+        <p2>${task.description}</p2>
         <button class="remove">x</button>
     `;
 
-    const remove = task.querySelector(".remove");
+    const checkbox = taskItem.querySelector("input");
+    const removeButton = taskItem.querySelector(".remove");
 
-    remove.addEventListener("click", () => {
-        removeTask(taskIndex)
+    checkbox.checked = task.completed;
+
+    checkbox.addEventListener("change", () => {
+        tasks[taskIndex].completed = checkbox.checked;
+        storeTasks();
     });
 
-    return task;
+    removeButton.addEventListener("click", () => {
+        removeTask(taskIndex);
+    });
+
+    return taskItem;
 }
 
 function updateTask() {
     taskList.innerHTML = "";
     tasks.forEach((task, taskIndex) => {
-        taskItem = createTask(task, taskIndex)
+        const taskItem = createTask(task, taskIndex);
         taskList.append(taskItem);
-    })
+    });
 }
 
 function removeTask(taskIndex) {
-    tasks = tasks.filter((_, i)=> i !== taskIndex);
+    tasks = tasks.filter((_, i) => i !== taskIndex);
+
     storeTasks();
     updateTask();
+
     return tasks;
 }
 
-// store tasks
+// Store tasks
 function storeTasks() {
     const tasksJSON = JSON.stringify(tasks);
     localStorage.setItem("tasks", tasksJSON);
 }
 
+// Fetch tasks
 function fetchTasks() {
     const tasks = localStorage.getItem("tasks");
-    return JSON.parse(tasks);
+    if (tasks) {
+        return JSON.parse(tasks);
+    } else {
+        return [];
+    }
 }
+
+updateTask();
